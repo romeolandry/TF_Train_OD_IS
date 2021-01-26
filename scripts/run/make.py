d456fa9e57ab2c14e1c69876a6e9d225abb72dad
romeolanry <kamgoche@yahoo.com>
2021-01-26 19:08:40 +0100

    refractory; command script

--- scripts/run/make.py
+++ scripts/run/make.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 import tensorflow as tf
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 # os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
@@ -12,27 +12,33 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 sys.path.append(os.path.abspath(os.curdir))
 
 from configs.run_config import *
-from scripts.api_scrpit import *
+from object_detection import *
+#from scripts.api_scrpit import *
+
+'''    
+    Return: True if proceed without error
+            False else.
+    The path to cloned model/research(object_detection is set into run_config
+'''
 
 def make_preprocessing ():
-    
-    """
-        This function run create_coco_tf_record.py as subprocess
-        Return: True if proceed without error
-            False else.
-    """
-    command = 'python scripts/api_scrpit/create_coco_tf_record.py '
 
-    Train_imgs = os.path.join(PATH_IMAGES,'train'+str(COCO_YEARS))
-    Test_Imgs = os.path.join(PATH_IMAGES,'test'+str(COCO_YEARS))
+    # get create_coco_tf_record.py path
+    create_coco_tf_record = os.path.join(Path_to_objection_dir,'dataset_tools/create_coco_tf_record.py')
+    if not os.path.exists(create_coco_tf_record):
+        sys.stderr.write("Please set the correct path to object_detection dir in the file run_config")
+    command = 'python ' + create_coco_tf_record
+
+    Train_images = os.path.join(PATH_IMAGES,'train'+str(COCO_YEARS))
+    Test_Images = os.path.join(PATH_IMAGES,'test'+str(COCO_YEARS))
     Val_img = os.path.join(PATH_IMAGES,'val'+str(COCO_YEARS))
 
     Train_ann = os.path.join(PATH_ANNOTATIONS,'instances_train'+str(COCO_YEARS)+ '.json')
     Test_ann = os.path.join(PATH_ANNOTATIONS,'image_info_test-dev'+str(COCO_YEARS)+ '.json')
     Val_ann = os.path.join(PATH_ANNOTATIONS,'instances_val'+str(COCO_YEARS)+ '.json')
 
-    arguments = '--logtostder --train_image_dir '+ Train_imgs +' --val_image_dir '\
-        + Val_img +' --test_image_dir '+ Test_Imgs + ' --train_annotations_file '+ Train_ann + \
+    arguments = ' --logtostder --train_image_dir '+ Train_images +' --val_image_dir '\
+        + Val_img +' --test_image_dir '+ Test_Images + ' --train_annotations_file '+ Train_ann + \
             ' --val_annotations_file ' + Val_ann + ' --testdev_annotations_file '+ Test_ann + \
                 ' --output_dir '+ os.path.join(PATH_ANNOTATIONS) 
 
@@ -49,7 +55,8 @@ def make_eval_on_train(model_name):
     Execute the model_main_tf2.py provided by the Api to Evaluate a selected model.
     the trained model will be save into model/ directory. this will create if not exist.
     """
-    # check if dirctory exist
+
+    # check if directory exist
     model_url = LIST_MODEL_TO_DOWNLOAD[model_name] # get model url from
     file_name = (model_url.split("/")[-1]).split(".")[0] # file name from url
     file_name = file_name.split('.')[0] # get file name without extension
@@ -68,8 +75,13 @@ def make_eval_on_train(model_name):
     path_to_pipeline = os.path.join(PATH_PRE_TRAINED_MODELS,file_name,'pipeline.config')
     path_to_save_trained_model = os.path.join(PATH_TRAINED_MODELS,new_file_name)
     
-    command = 'python scripts/api_scrpit/model_main_tf2.py '
-    arguments = '--model_dir='+ path_to_save_trained_model +' --pipeline_config_path='+ path_to_pipeline + \
+    # get create_coco_tf_record.py path
+    model_main_tf2 = os.path.join(Path_to_objection_dir,'model_main_tf2.py')
+    if not os.path.exists(model_main_tf2):
+        sys.stderr.write("Please set the correct path to object_detection dir in the file run_config")
+    command = 'python ' + model_main_tf2
+
+    arguments = ' --model_dir='+ path_to_save_trained_model +' --pipeline_config_path='+ path_to_pipeline + \
         ' --num_train_steps='+ str(NUM_TRAIN_STEP) +' --checkpoint_dir=' + path_to_save_trained_model + \
         ' --eval'
 
@@ -103,9 +115,14 @@ def make_train(model_name):
     
     path_to_pipeline = os.path.join(PATH_PRE_TRAINED_MODELS,file_name,'pipeline.config')
     path_to_save_trained_model = os.path.join(PATH_TRAINED_MODELS,new_file_name)
-    
-    command = 'python scripts/api_scrpit/model_main_tf2.py '
-    arguments = '--model_dir='+ path_to_save_trained_model +' --pipeline_config_path='+ path_to_pipeline + \
+
+    # get create_coco_tf_record.py path
+    model_main_tf2 = os.path.join(Path_to_objection_dir,'model_main_tf2.py')
+    if not os.path.exists(model_main_tf2):
+        sys.stderr.write("Please set the correct path to object_detection dir in the file run_config")
+    command = 'python ' + model_main_tf2
+
+    arguments = ' --model_dir='+ path_to_save_trained_model +' --pipeline_config_path='+ path_to_pipeline + \
         ' --num_train_steps='+ str(NUM_TRAIN_STEP) + ' --checkpoint_every_n='+ str(CHECKPOINT_EVERY_N_STEP) + \
         ' --train'
     try:
@@ -140,11 +157,16 @@ def make_export(model_name):
     if not os.path.exists(os.path.join(PATH_TO_EXPORT_DIR,new_file_name)):
         if not os.path.exists(PATH_TO_EXPORT_DIR):
             os.mkdir(PATH_TO_EXPORT_DIR)
-        os.mkdir(os.path.join(PATH_TO_EXPORT_DIR,new_file_name))
-    
+        os.mkdir(os.path.join(PATH_TO_EXPORT_DIR,new_file_name))    
     output_dir = os.path.join(PATH_TO_EXPORT_DIR,new_file_name)
-    command = 'python scripts/api_scrpit/exporter_main_v2.py '
-    arguments ='--input_type '+ INPUT_TYPE[0] + ' --pipeline_config_path=' + path_to_pipeline + ' --trained_checkpoint_dir=' \
+
+    # get create_coco_tf_record.py path
+    exporter_main_v2 = os.path.join(Path_to_objection_dir,'exporter_main_v2.py')
+    if not os.path.exists(exporter_main_v2):
+        sys.stderr.write("Please set the correct path to object_detection dir in the file run_config")
+    command = 'python ' + exporter_main_v2
+
+    arguments =' --input_type '+ INPUT_TYPE[0] + ' --pipeline_config_path=' + path_to_pipeline + ' --trained_checkpoint_dir=' \
         + trained_checkpoint_dir + ' --output_directory=' +  output_dir
 
     try:

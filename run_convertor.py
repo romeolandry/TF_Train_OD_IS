59894a15a716d5095264363d226fdc910ce10134
romeolanry <kamgoche@yahoo.com>
2021-01-26 18:04:19 +0100

    refractory; update arguments

--- run_convertor.py
+++ run_convertor.py
@@ -1,30 +1,32 @@
+import argparse
+import click
+import os
+
 from configs.run_config import *
 from scripts.run.download import *
 from scripts.run.make import *
 from scripts.Evaluation.utils import *
 from scripts.Evaluation.convertor import Convertor
 
-import argparse
-
 # convert
 parser = argparse.ArgumentParser(description="Choose what you won beetwen [Data_preprocessing, Train, Evaluate]")
 
 
-parser.add_argument("-t","--type", choices=['tftrt','trt'],
+parser.add_argument("-t","--type", choices=['tf-trt','tf-infer'],
     required=True,
-    help="would you convert to UFF_TRT or TF_TRTR")
-
-parser.add_argument("--output_model_dir", default=PATH_TO_CONVERTED_MODELS,
-    help="choose TF-TRT or TRT")
-
-parser.add_argument("--precision_mode", default= PRECISION_MODE,
-    help="precision mode for tensorrt model")
-
-parser.add_argument("--max_ws", default=MAX_WORSPACE_SIZE_BITES,
-    help="MAX_WORSPACE_SIZE_BITES for tenssorRT model")
+    help="get a  tf model for inference or tf-trt for inference")
 
 parser.add_argument("-p","--path", required=True,
-    help="path to model to convert .h5 or tensorflow model")
+    help="path to model to convert .h5 or tensorflow model directory (saved_model)")
+
+parser.add_argument("--output_model_dir", default=PATH_TO_CONVERTED_MODELS,
+    help="where the converted model will be saved by default the model.")
+
+parser.add_argument("--mode", default= PRECISION_MODE,
+    help="precision mode if you won a tf-trt model")
+
+parser.add_argument("--max_ws", default=MAX_WORKSPACE_SIZE_BITES,
+    help="MAX_WORKSPACE_SIZE_BITES for tf-trt model")
 
 parser.add_argument("--input_size", default=640,
     help="Input size of image for eventual calibration")
@@ -34,14 +36,15 @@ parser.add_argument("--batch_size", default=32,
 
 
 def main(args):
-    if args.type =="trt":
-        click.echo(click.style(f"\n Conversion of {args.path} to TensorRT using Uff parser model \n", bg='green', bold=True, fg='white'))
+    if args.type =="tf-infer":
+        click.echo(click.style(f"\n Conversion of {args.path} to Tensorflow inference model  \n", bold=True, fg='green'))
+        
     else:
-        click.echo(click.style(f"\n Conversion of {args.path} to Tensorflow-TensorRT model \n", bg='green', bold=True, fg='white'))
-        if ((args.precision_mode == "INT8") or (args.precision_mode == "int8")):
+        click.echo(click.style(f"\n Conversion of {args.path} to Tensorflow-TensorRT model \n", bold=True, fg='green'))
+        if ((args.mode == "INT8") or (args.mode == "int8")):
             click.echo(click.style(f"\n to calibrate you model the input image size should be the same as the input size for original model \t", bold=True, fg='white'))
             click.echo(click.style(f"\n to change the default image size set --input_size parameter \t", bold=True, fg='white'))
-            click.echo(click.style(f"\n the calibration will be don wiht an image size of: {args.input_size} x {args.input_size} do you won to continuous ?", bold=True, fg='white'))
+            click.echo(click.style(f"\n the calibration will be don with an image size of: {args.input_size} x {args.input_size} do you won to continuous ?", bold=True, fg='white'))
             
             c = click.getchar()
             click.echo()
@@ -51,18 +54,18 @@ def main(args):
 
         batched_input = batch_input(batch_size=args.batch_size, input_size=[args.input_size,args.input_size,3], path_to_test_img_dir='images/test2017')
 
-        con = Convertor(args.path,args.precision_mode,args.max_ws)
+        con = Convertor(args.path,args.mode,args.max_ws)
         original_path,saved_model_path = con.convert_to_TF_TRT_graph_and_save(calibration_data=batched_input)
 
         # save performance
-        if args.type == 'tftrt':
+        if args.type == 'tf-trt':
             mode = 'TensorFlow-TensorRT'
         else:
             mode = 'UFF_Parser'
         value = {
             'ORIGINAL': original_path,
-            'PRECISION_MODE': args.precision_mode,
-            'MAX_WORSPACE_SIZE_BITES': args.max_ws,
+            'PRECISION_MODE': args.mode,
+            'MAX_WORKSPACE_SIZE_BITES': args.max_ws,
             'CONVERSION_TYPE': mode,
             'CONVERTED_MODEL':saved_model_path
         }
@@ -70,7 +73,7 @@ def main(args):
         to_save = {
             model_name:value,
         }
-        save_perfromance('convertor',to_save)
+        save_performance('convertor',to_save)
 
 
 if __name__ == "__main__":
